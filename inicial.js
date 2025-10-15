ffd1807202d7e687f87ae7b3b837f8dd2def65ce
Nikolly <nikollygabrieli02@gmail.com>
2025-10-15 13:04:46 -0300

    Adiciona página inicial com HTML, CSS, JS e imagens

---
+++ inicial.js
@@ -0,0 +1,5 @@
+const compraButton = document.querySelector('.comprar-ingressos-btn');
+
+compraButton.addEventListener('click', () => {
+    alert('Você será redirecionado para a página de compra. Bom filme!');
+});
