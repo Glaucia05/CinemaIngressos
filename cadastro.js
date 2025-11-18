9293a8de629ba6e8829936f03c5227c5728e8042
glaucia <glauciabispo040505@gmail.com>
2025-11-18 15:03:20 -0300

    adicionando inyeração basica com Js

---
+++ cadastro.js
@@ -0,0 +1,27 @@
+
+//Aplica interatividado do botão
+function porCima(obj) {
+
+obj.style.transform = "scale(1.15)";
+
+
+}
+
+function porFora(obj) {
+
+obj.style.transform = "scale(1)";
+}
+
+function PorCima2(obj){
+obj.style.borderColor = "blue";
+}
+
+
+function PorFora2(obj){
+obj.style.borderColor = "black";
+    
+}
+function pegarEntradas(){
+    nome = document.getElementById("nome")
+}
+
