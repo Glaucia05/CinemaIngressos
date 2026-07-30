e451b70f242ab11868ec56224445cd25890fdb6d
glaucia bispo <glauciabispo040505@gmail.com>
2026-07-30 20:21:29 -0300

    feat: iniciando contrução de verificação de usuario

--- src/cadastro/cadastro.php
+++ src/cadastro/cadastro.php
@@ -6,11 +6,15 @@ $Email=$_POST['email'];
 $Senha=$_POST['senha'];
 $Telefone=$_POST['telefone'];
 
+$hash = password_hash($Senha, PASSWORD_DEFAULT);
 
 $result = mysqli_query($conexao, "INSERT INTO cadastro(Nome, Email, Senha, Telefone) 
-                                    VALUES('$Nome','$Email','$Senha','$Telefone' )");
+                                    VALUES('$Nome','$Email','$hash','$Telefone' )");
 
-/* 
+
+
+
+/* TESTE DE ENVIO DE DADOS PARA O BD
 if ($result) {
     header("Location: ../inicio/inicio.html");
     exit;
--- src/inicio/inicio.php
+++ src/inicio/inicio.php
@@ -1,17 +1,28 @@
 <?php
-//Validar dados do login
-//function ValidarEmail($Email){
-//if(filter_var($Email,FILTER_VALIDATE_EMAIL)){
-  //  return true;
-//}else{
-  ///  return false;
-//}
-//}
+
+include_once("../../conexao.php");
 
 
-//if(!ValidarEmail()){
-  //  echo 'Email invalido';
-//}
+$Email = $_POST['email'] ?? ''; 
+$Senha = $_POST['senha'] ?? '';
+
+ // 1. Buscar o usuário no banco 
+$Senha_banco = mysqli_query($conexao, "SELECT Senha FROM cadastro WHERE Email = '$Email'");
+// valor de exemplo 
+
+$dado = mysqli_fetch_assoc($Senha_banco);
+
+// 2. Verificar a senha 
+if (password_verify($Senha, $dado['Senha'])) {    
+	// Senha correta    
+	echo "Login permitido";  
+
+	// Aqui você inicia a sessão, salva dados na session, etc. 
+  
+} else {    
+	// Senha incorreta    
+	echo "E-mail ou senha inválidos"; 
+}
 
 
 
