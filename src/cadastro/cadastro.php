e6bd643a6802534c0b7968d4a164dec694f27cfa
glaucia bispo <glauciabispo040505@gmail.com>
2026-07-30 21:39:57 -0300

    adicionando algumas validações de entrada em PHP

--- src/cadastro/cadastro.php
+++ src/cadastro/cadastro.php
@@ -5,11 +5,35 @@ $Nome=$_POST['nome'];
 $Email=$_POST['email'];
 $Senha=$_POST['senha'];
 $Telefone=$_POST['telefone'];
+$senhaConfirme = $_POST['senhaConfirme'];
 
-$hash = password_hash($Senha, PASSWORD_DEFAULT);
+if (!empty($Nome) && !empty($Senha)) {
+    // Verificação de igualdade (Case Sensitive)
+    if ($Nome === $Senha ) {
+        echo "<h2>Entrada inválida!</h2>";
+        echo "<p>Por segurança, a senha não pode ser igual ao nome de usuário.</p>";
+        echo "<a href='cadastro.html'>Voltar para o cadastro</a>";
+        exit();
+    }
+    
+    if($senhaConfirme !== $Senha ){
+        echo "<h2>Entrada inválida!</h2>";
+        echo "As senhas devem ser iguais!";
+        echo "<a href='cadastro.html'>Voltar para o cadastro</a>";
+        exit();
 
-$result = mysqli_query($conexao, "INSERT INTO cadastro(Nome, Email, Senha, Telefone) 
+    } 
+    else {
+            echo 'Entrada valida';
+            $hash = password_hash($Senha, PASSWORD_DEFAULT);
+            $result = mysqli_query($conexao, "INSERT INTO cadastro(Nome, Email, Senha, Telefone) 
                                     VALUES('$Nome','$Email','$hash','$Telefone' )");
+    }
+}
+
+
+
+
 
 
 
